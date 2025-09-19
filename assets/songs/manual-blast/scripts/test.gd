1dc08c54656273ad4958b0ba9d25bad8cbea9a01
lavy cat <88630349+lavycat@users.noreply.github.com>
2025-09-19 03:43:34 -0400

    minimalized

--- assets/songs/manual-blast/scripts/test.gd
+++
@@ -1,3 +0,0 @@
-extends FunkinScript
-#func _process(delta: float) -> void:
-	#Conductor.rate = 1.0 + cos(Conductor.beat*9)*0.05
--- scenes/game/characters/dbtg_run.gd
+++
@@ -1,4 +0,0 @@
-extends Character
-func  _process(delta: float) -> void:
-	super(delta)
-	$sprite.frame  = $sprite2.frame%9
